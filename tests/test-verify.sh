4f781a153cf300cfed623ac33c12c24694ed2632
Adam Dyson <contact@adamdyson.com.au>
2017-09-28 19:25:13 +1000

    Simplified testing role using Travis CI and docker containers.

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -2,5 +2,5 @@
 
 # Verify the hostname and hosts entries.
 docker exec --tty ${container_id} env TERM=xterm hostname
-docker exec --tty ${container_id} env TERM=xterm getent hosts localhost
-docker exec --tty ${container_id} env TERM=xterm getent hosts database
+docker exec --tty ${container_id} env TERM=xterm getent hosts 127.0.0.1
+docker exec --tty ${container_id} env TERM=xterm getent hosts 192.168.66.10
